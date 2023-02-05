ed713b0751dff9a57f3a87e94d96212227bf75ba
Albert Johannessen <albertjohannessen@live.no>
2023-02-05 22:11:11 +0100

    Better folder structure, PDE PINN still not working quite as it should

---
+++ Experiments/Heat1D/pinn_heat_1d.py
@@ -0,0 +1,216 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import jax
+import jax.numpy as jnp
+import optax
+
+seed = 42069
+random.seed(seed)
+np.random.seed(seed)
+key = jax.random.PRNGKey(seed)
+
+
+def heat(t, x):
+    return jnp.sin(jnp.pi * x) * jnp.exp(-jnp.pi**2 * t)
+
+
+def generate_data(function, T0, T1, X0, X1, batch_size, key):
+    key1, key2, key3, key4 = jax.random.split(key, 4)
+
+    t_initial = jnp.ones((batch_size // 2, 1)) * T0
+    x_initial = jax.random.uniform(key1, shape=(batch_size // 2, 1), minval=X0, maxval=X1)
+    u_initial = function(T0, x_initial)
+
+    t_boundary = jax.random.uniform(key2, shape=(batch_size // 2, 1), minval=T0, maxval=T1)
+    x_boundary0 = jnp.ones((batch_size // 4, 1)) * X0
+    x_boundary1 = jnp.ones((batch_size // 4, 1)) * X1
+    x_boundary = jnp.concatenate((x_boundary0, x_boundary1), axis=0)
+    u_boundary = function(t_boundary, x_boundary)
+
+    t_ib = jnp.concatenate((t_initial, t_boundary), axis=0)
+    x_ib = jnp.concatenate((x_initial, x_boundary), axis=0)
+    u_ib = jnp.concatenate((u_initial, u_boundary), axis=0)
+
+    t_interior = jax.random.uniform(key3, shape=(batch_size, 1), minval=T0, maxval=T1)
+    x_interior = jax.random.uniform(key4, shape=(batch_size, 1), minval=X0, maxval=X1)
+    u_interior = function(t_interior, x_interior)
+
+    return t_ib, x_ib, u_ib, t_interior, x_interior, u_interior
+
+
+def data_split(x, ratio=0.8):
+    batch_size = x.shape[0]
+    x_train = x[:int(batch_size * ratio), :]
+    x_val = x[int(batch_size * ratio):, :]
+    return x_train, x_val
+
+
+def model_init(model_def, key):
+    subkeys = jax.random.split(key, num=(len(model_def) - 1) * 2)
+    params = []
+    for i in range(len(model_def) - 1):
+        layer = {
+            "weights": jax.random.normal(subkeys[i], (model_def[i], model_def[i + 1])),
+            "bias": jax.random.normal(subkeys[i + len(model_def) - 1], (model_def[i + 1],))
+        }
+        params.append(layer)
+    return params
+
+
+key, subkey = jax.random.split(key)
+model_def = [2, 50, 100, 50, 1]
+params = model_init(model_def, subkey)
+
+
+optimizer = optax.adamw(learning_rate=1e-3)
+opt_state = optimizer.init(params)
+
+
+def model_forward(t, x, params):
+    x = jnp.concatenate((t, x), axis=0)
+    for i in range(len(params)):
+        weights = params[i]["weights"]
+        bias = params[i]["bias"]
+        x = x @ weights + bias
+        if i < len(params) - 1:
+            x = jax.nn.sigmoid(x)
+    return x[0]
+
+
+u = jax.vmap(model_forward, in_axes=(0, 0, None))
+u_t = jax.vmap(jax.grad(model_forward, argnums=0), in_axes=(0, 0, None))
+u_xx = jax.vmap(jax.jacfwd(jax.jacrev(model_forward, argnums=1), argnums=1), in_axes=(0, 0, None))
+
+
+def pinn(t, x, params):
+    f1 = u_t(t, x, params).squeeze()
+    f2 = u_xx(t, x, params).squeeze()
+    f = f1 - f2
+    return f
+
+
+def u_loss(u_pred, u_true):
+    return jnp.mean((u_pred - u_true)**2)
+
+
+def f_loss(f):
+    return jnp.mean(f**2)
+
+
+def loss(u_pred, u_true, f, beta=1.0):
+    return u_loss(u_pred, u_true) + beta * f_loss(f)
+
+
+@jax.value_and_grad
+def train_step(params, t_ib, x_ib, u_ib, t_f, x_f):
+    u_pred = u(t_ib, x_ib, params)
+    f = pinn(t_f, x_f, params)
+    return loss(u_pred, u_ib, f)
+
+
+@jax.jit
+def train(params, opt_state, t_ib, x_ib, u_ib, t_f, x_f):
+    loss_value, loss_grads = train_step(params, t_ib, x_ib, u_ib, t_f, x_f)
+    updates, opt_state = optimizer.update(loss_grads, opt_state, params)
+    params = optax.apply_updates(params, updates)
+    return loss_value, params, opt_state
+
+
+@jax.jit
+def validate(params, t_interior, x_interior, u_interior, t_f, x_f):
+    u_pred = u(t_interior, x_interior, params)
+    f = pinn(t_f, x_f, params)
+    return loss(u_pred, u_interior, f)
+
+
+def plot_u():
+    n = 100
+    t = np.linspace(T0, T1, n)
+    x = np.linspace(X0, X1, n)
+    tt = np.zeros((n * n, 1))
+    xx = np.zeros((n * n, 1))
+    for i in range(n):
+        for j in range(n):
+            tt[i * n + j, 0] = t[i]
+            xx[i * n + j, 0] = x[j]
+    uu = u(tt, xx, params)
+    uu_true = heat(tt, xx)
+    print(jnp.mean((uu - uu_true)**2))
+    # exit()
+    un = np.zeros((n, n))
+    for i in range(n):
+        for j in range(n):
+            un[j, i] = uu[i * n + j]
+            # un[j, i] = uu_true[i * n + j]
+    plt.figure()
+    plt.pcolormesh(t, x, un)
+    plt.colorbar()
+    plt.show()
+
+
+def plot_loss(train_losses, val_losses):
+    train_losses = train_losses[train_losses > 0]
+    val_losses = val_losses[val_losses > 0]
+    epochs = max(train_losses.shape[0], val_losses.shape[0])
+    plt.figure()
+    plt.plot(np.arange(1, epochs + 1, 1), train_losses)
+    plt.plot(np.arange(1, epochs + 1, 1), val_losses)
+    plt.legend(["Training loss", "Validation loss"])
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.title("")
+    plt.yscale("log")
+    plt.grid()
+    plt.show()
+
+
+function = heat
+T0 = 0.0
+T1 = 0.2
+X0 = 0.0
+X1 = 1.0
+batch_size = 100
+key, subkey = jax.random.split(key)
+t_ib, x_ib, u_ib, t_interior, x_interior, u_interior = generate_data(function, T0, T1, X0, X1, batch_size, subkey)
+n_f = 100
+t_f = jnp.expand_dims(jnp.linspace(T0, T1, n_f), 1)
+x_f = jnp.expand_dims(jnp.linspace(X0, X1, n_f), 1)
+
+t_i = t_ib[:batch_size // 2, :]
+x_i = x_ib[:batch_size // 2, :]
+u_i = u_ib[:batch_size // 2, :]
+
+t_b = t_ib[batch_size // 2:, :]
+x_b = x_ib[batch_size // 2:, :]
+u_b = u_ib[batch_size // 2:, :]
+
+epochs = 10000
+train_losses = np.zeros(epochs)
+val_losses = np.zeros(epochs)
+for epoch in range(1, epochs + 1):
+    try:
+        t_train_i = jnp.expand_dims(t_i[epoch % (batch_size // 2), :], axis=0)
+        x_train_i = jnp.expand_dims(x_i[epoch % (batch_size // 2), :], axis=0)
+        u_train_i = jnp.expand_dims(u_i[epoch % (batch_size // 2), :], axis=0)
+
+        t_train_b = jnp.expand_dims(t_b[epoch % (batch_size // 2), :], axis=0)
+        x_train_b = jnp.expand_dims(x_b[epoch % (batch_size // 2), :], axis=0)
+        u_train_b = jnp.expand_dims(u_b[epoch % (batch_size // 2), :], axis=0)
+
+        t_train = jnp.concatenate((t_train_i, t_train_b), axis=0)
+        x_train = jnp.concatenate((x_train_i, x_train_b), axis=0)
+        u_train = jnp.concatenate((u_train_i, u_train_b), axis=0)
+
+        train_loss, params, opt_state = train(params, opt_state, t_train, x_train, u_train, t_f, x_f)
+        val_loss = validate(params, t_interior, x_interior, u_interior, t_f, x_f)
+        train_losses[epoch - 1] = train_loss
+        val_losses[epoch - 1] = val_loss
+
+        print(f"Epoch: {epoch:3d}, Train Loss: {train_loss.item():.5f}, Val Loss: {val_loss.item():.5f}")
+    except KeyboardInterrupt:
+        break
+
+# plot_loss(train_losses, val_losses)
+plot_u()
+
---
+++ Experiments/Riccati/pinn_riccati.py
@@ -0,0 +1,177 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import jax
+import jax.numpy as jnp
+import optax
+import diffrax
+
+
+seed = 42069
+random.seed(seed)
+np.random.seed(seed)
+key = jax.random.PRNGKey(seed)
+
+
+def riccati(t, x, args):
+    return x * x - t
+
+
+def generate_data(function, args, key):
+    T0 = 0.0
+    T1 = 10.0
+    h = 0.01
+    t = jnp.arange(T0, T1 + h, h)
+    saveat = diffrax.SaveAt(ts=t)
+    x0 = jax.random.uniform(key, minval=-5.0, maxval=0.0)
+    term = diffrax.ODETerm(function)
+    solver = diffrax.Dopri5()
+    solution = diffrax.diffeqsolve(term, solver, t0=T0, t1=T1, dt0=h, y0=x0, saveat=saveat, args=args, adjoint=diffrax.NoAdjoint())
+    return t, solution.ys
+
+
+def data_split(x, ratio=0.8):
+    batch_size = x.shape[0]
+    x_train = x[:int(batch_size * ratio)]
+    x_val = x[int(batch_size * ratio):]
+    return x_train, x_val
+
+
+def model_init(model_def, key):
+    subkeys = jax.random.split(key, num=(len(model_def) - 1) * 2)
+    params = []
+    for i in range(len(model_def) - 1):
+        layer = {
+            "weights": jax.random.normal(subkeys[i], (model_def[i], model_def[i + 1])),
+            "bias": jax.random.normal(subkeys[i + len(model_def) - 1], (model_def[i + 1],))
+        }
+        params.append(layer)
+    return params
+
+
+key, subkey = jax.random.split(key)
+model_def = [1, 50, 100, 100, 50, 1]
+params = model_init(model_def, subkey)
+
+
+optimizer = optax.adamw(learning_rate=1e-3)
+opt_state = optimizer.init(params)
+
+
+def model_forward(x, params):
+    for i in range(len(params)):
+        weights = params[i]["weights"]
+        bias = params[i]["bias"]
+        x = x @ weights + bias
+        if i < len(params) - 1:
+            x = jax.nn.sigmoid(x)
+    return x
+
+
+model_x = jax.vmap(model_forward, in_axes=(0, None))
+model_x_t = jax.vmap(jax.jacfwd(model_forward), in_axes=(0, None))
+
+
+def pinn(t, params):
+    f1 = model_x_t(t, params)
+    f2 = model_x(t, params)**2 - t
+    f = f1 - f2
+    return f
+
+
+def u_loss(x_pred, x_true):
+    return jnp.mean((x_pred - x_true)**2)
+
+
+def f_loss(f):
+    return jnp.mean(f**2)
+
+
+def loss(x_pred, x_true, f, beta=0.01):
+    return u_loss(x_pred, x_true) + beta * f_loss(f)
+
+
+@jax.value_and_grad
+def train_step(params, t, t_boundary, x_boundary):
+    x_pred = model_x(t_boundary, params)
+    f = pinn(t, params)
+    return loss(x_pred, x_boundary, f)
+
+
+@jax.jit
+def train(params, opt_state, t, t_boundary, x_boundary):
+    loss_value, loss_grads = train_step(params, t, t_boundary, x_boundary)
+    updates, opt_state = optimizer.update(loss_grads, opt_state, params)
+    params = optax.apply_updates(params, updates)
+    return loss_value, params, opt_state
+
+
+@jax.jit
+def validate(params, t, t_interior, x_interior):
+    x_pred = model_x(t_interior, params)
+    f = pinn(t, params)
+    return loss(x_pred, x_interior, f)
+
+
+def plot_x():
+    n = 100
+    t = jnp.linspace(0, 10, n)
+    t = jnp.expand_dims(t, 1)
+    x = model_x(t, params)
+    plt.figure()
+    plt.plot(t_batch, x_batch)
+    plt.plot(t, x)
+    plt.xlabel(r"$t$")
+    plt.ylabel(r"$x(t)$")
+    plt.legend(["True system", "Learned system"])
+    plt.show()
+
+
+def plot_loss(train_losses, val_losses):
+    train_losses = train_losses[train_losses > 0]
+    val_losses = val_losses[val_losses > 0]
+    epochs = max(train_losses.shape[0], val_losses.shape[0])
+    plt.figure()
+    plt.plot(np.arange(1, epochs + 1, 1), train_losses)
+    plt.plot(np.arange(1, epochs + 1, 1), val_losses)
+    plt.legend(["Training loss", "Validation loss"])
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.title("")
+    plt.yscale("log")
+    plt.grid()
+    plt.show()
+
+
+key, subkey = jax.random.split(key)
+function = riccati
+args = None
+t_batch, x_batch = generate_data(function, None, subkey)
+t_batch = jnp.expand_dims(t_batch, 1)
+x_batch = jnp.expand_dims(x_batch, 1)
+# t_boundary = jnp.stack((t_batch[0], t_batch[-1]), axis=0)
+# t_interior = t_batch[1:-1, :]
+t_boundary = jnp.array((t_batch[0],))
+t_interior = t_batch[1:, :]
+# x_boundary = jnp.stack((x_batch[0], x_batch[-1]), axis=0)
+# x_interior = x_batch[1:-1, :]
+x_boundary = jnp.array((x_batch[0],))
+x_interior = x_batch[1:, :]
+
+epochs = 1000
+train_losses = np.zeros(epochs)
+val_losses = np.zeros(epochs)
+for epoch in range(1, epochs + 1):
+    try:
+        train_loss, params, opt_state = train(params, opt_state, t_batch, t_boundary, x_boundary)
+        val_loss = validate(params, t_batch, t_interior, x_interior)
+        train_losses[epoch - 1] = train_loss
+        val_losses[epoch - 1] = val_loss
+
+        print(f"Epoch: {epoch:3d}, Train Loss: {train_loss.item():.4f}, Val Loss: {val_loss.item():.4f}")
+    except KeyboardInterrupt:
+        break
+
+# plot_loss(train_losses, val_losses)
+plot_x()
+
